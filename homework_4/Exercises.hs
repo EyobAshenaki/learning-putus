4bec1f2ccde74d616971952c0cf8a5d1821619d0
EyobAshenaki <ethioeyoba@gmail.com>
2023-03-25 05:59:46 +0300

    feat(homework_4): add solution for exercise 3 & 4
    - Fixed the solution for question 2

--- homework_4/Exercises.hs
+++ homework_4/Exercises.hs
@@ -1,3 +1,5 @@
+import Data.List ((\\))
+
 -- ********** Exercise 1 **********
 
 fun1 :: [Integer] -> Integer
@@ -14,7 +16,7 @@ data Tree a
   deriving (Show, Eq)
 
 foldTree :: Ord a => Eq a => [a] -> Tree a
-foldTree = foldr insert' Leaf
+foldTree = foldr insert Leaf
 
 -- checks if the tree has a leaf as an immediate child
 hasLeaf :: Tree a -> Bool
@@ -26,18 +28,31 @@ height :: Tree a -> Integer
 height Leaf = -1
 height (Node h _ _ _) = h
 
-insert :: Ord a => a -> Tree a -> Tree a
-insert x Leaf = Node 0 Leaf x Leaf
-insert x wholeT@(Node h leftT val rightT)
-  | hasLeaf wholeT = if leftT == Leaf then Node (h + 1) (insert x leftT) val rightT else Node (h + 1) leftT val (insert x rightT)
-  | otherwise = if height leftT < height rightT then Node h (insert x leftT) val rightT else Node h leftT val (insert x rightT)
-
 -- inserts a value into the tree in the correct position based on the height of the left and right subtrees
-insert' :: Ord a => a -> Tree a -> Tree a
-insert' = go 0
+insert :: Ord a => a -> Tree a -> Tree a
+insert = go 0
   where
-    go :: Ord a => Integer -> a -> Tree a -> Tree a
     go h x Leaf = Node h Leaf x Leaf
     go h x whole@(Node h' leftT val rightT)
-      | hasLeaf whole = if leftT == Leaf then Node (h' + 1) (go h x leftT) val rightT else Node (h' + 1) leftT val (go h x rightT)
-      | otherwise = if height leftT < height rightT then Node (max h h + 1) (go h x leftT) val rightT else Node (max h h + 1) leftT val (go h x rightT)
+      | hasLeaf whole = if leftT == Leaf then Node nextHeight (go h x leftT) val rightT else Node nextHeight leftT val (go h x rightT)
+      | otherwise = if height leftT < height rightT then Node nextHeight (go h x leftT) val rightT else Node nextHeight leftT val (go h x rightT)
+      where
+        nextHeight = 1 + max (height leftT) (height rightT)
+
+-- ********** Exercise 3 **********
+
+xor :: [Bool] -> Bool
+xor = odd . length . foldr (\x acc -> if x then x : acc else acc) []
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x acc -> f x : acc) []
+
+myFoldl :: (a -> b -> a) -> a -> [b] -> a
+myFoldl f base xs = foldr (\x acc -> f acc x) base (reverse xs)
+
+-- ********** Exercise 4 **********
+
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram n = map (\x -> 2 * x + 1) $ [1 .. n] \\ cartProd n
+  where
+    cartProd n = [x + y + (2 * x * y) | x <- [1 .. n], y <- [1 .. n], x < y]
