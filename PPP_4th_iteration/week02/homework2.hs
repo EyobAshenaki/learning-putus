7faefde3b217fc34e4dbf93670db80eb411492c9
EyobAshenaki <ethioeyoba@gmail.com>
2023-04-05 13:04:35 +0300

    feat(4th/week02): add solution for homework 2

---
+++ PPP_4th_iteration/week02/homework2.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Homework2 where
+
+import Plutus.V2.Ledger.Api qualified as PlutusV2
+import PlutusTx (unstableMakeIsData)
+import PlutusTx.Prelude (Bool, BuiltinData)
+import Prelude (undefined)
+
+-- import           Utilities            (wrapValidator)
+
+---------------------------------------------------------------------------------------------------
+----------------------------------- ON-CHAIN / VALIDATOR ------------------------------------------
+
+data MyRedeemer = MyRedeemer
+  { flag1 :: Bool,
+    flag2 :: Bool
+  }
+
+PlutusTx.unstableMakeIsData ''MyRedeemer
+
+{-# INLINEABLE mkValidator #-}
+-- Create a validator that unlocks the funds if MyRedemeer's flags are different
+mkValidator :: () -> MyRedeemer -> PlutusV2.ScriptContext -> Bool
+mkValidator _ (MyRedeemer {flag1 = f1, flag2 = f2}) _ = traceIfFalse "Wrong Redeemer" $ f1 /= f2
+
+{-# INLINEABLE wrappedVal #-}
+wrappedVal :: BuiltinData -> BuiltinData -> BuiltinData -> ()
+wrappedVal = wrapValidator mkValidator
+
+validator :: PlutusV2.Validator
+validator = PlutusV2.mkValidatorScript $$(PlutusTx.compile [||wrappedVal||])
