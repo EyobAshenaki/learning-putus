75e8a2c022bcc219b30ff86323bfad8d9abe2867
EyobAshenaki <ethioeyoba@gmail.com>
2023-04-27 09:09:07 +0300

    feat(4th/week05): add serialize on-chain code for hw-1

--- PPP_4th_iteration/week05/Homework1.hs
+++ PPP_4th_iteration/week05/Homework1.hs
@@ -1,42 +1,63 @@
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Homework1 where
 
-import           Plutus.V2.Ledger.Api (BuiltinData, MintingPolicy, POSIXTime,
-                                       PubKeyHash, ScriptContext,
-                                       mkMintingPolicyScript, TxInfo, txInfoValidRange, scriptContextTxInfo)
-import           Plutus.V2.Ledger.Contexts (txSignedBy)
-import qualified PlutusTx
-import           PlutusTx.Prelude     (Bool, ($), traceIfFalse, (&&))
-import           Utilities            (wrapPolicy)
 import Plutus.V1.Ledger.Interval (after)
+import Plutus.V2.Ledger.Api
+  ( BuiltinData,
+    MintingPolicy,
+    POSIXTime,
+    PubKeyHash,
+    ScriptContext,
+    TxInfo,
+    mkMintingPolicyScript,
+    scriptContextTxInfo,
+    txInfoValidRange,
+  )
+import Plutus.V2.Ledger.Contexts (txSignedBy)
+import qualified PlutusTx
+import PlutusTx.Prelude (Bool, traceIfFalse, ($), (&&))
+import Utilities (wrapPolicy, writeCodeToFile)
+import Prelude (IO)
 
-{-# INLINABLE mkDeadlinePolicy #-}
+{-# INLINEABLE mkDeadlinePolicy #-}
 -- This policy should only allow minting (or burning) of tokens if the owner of the specified PubKeyHash
 -- has signed the transaction and if the specified deadline has not passed.
 mkDeadlinePolicy :: PubKeyHash -> POSIXTime -> () -> ScriptContext -> Bool
-mkDeadlinePolicy _pkh _deadline () _ctx = traceIfFalse "*** Invalid signature ***" signedByValidSignature &&
-                                          traceIfFalse "*** Deadline passed ***" deadlineNotReached
-    where 
-        info :: TxInfo
-        info = scriptContextTxInfo _ctx
+mkDeadlinePolicy _pkh _deadline () _ctx =
+  traceIfFalse "*** Invalid signature ***" signedByValidSignature
+    && traceIfFalse "*** Deadline passed ***" deadlineNotReached
+  where
+    info :: TxInfo
+    info = scriptContextTxInfo _ctx
 
-        signedByValidSignature :: Bool
-        signedByValidSignature = txSignedBy info _pkh
+    signedByValidSignature :: Bool
+    signedByValidSignature = txSignedBy info _pkh
 
-        deadlineNotReached :: Bool
-        deadlineNotReached = after _deadline $ txInfoValidRange info
+    deadlineNotReached :: Bool
+    deadlineNotReached = after _deadline $ txInfoValidRange info
 
-{-# INLINABLE mkWrappedDeadlinePolicy #-}
-mkWrappedDeadlinePolicy :: PubKeyHash -> POSIXTime -> BuiltinData -> BuiltinData -> ()
-mkWrappedDeadlinePolicy pkh deadline = wrapPolicy $ mkDeadlinePolicy pkh deadline
+{-# INLINEABLE mkWrappedDeadlinePolicy #-}
+-- mkWrappedDeadlinePolicy :: PubKeyHash -> POSIXTime -> BuiltinData -> BuiltinData -> ()
+-- mkWrappedDeadlinePolicy pkh deadline = wrapPolicy $ mkDeadlinePolicy pkh deadline
+mkWrappedDeadlinePolicy :: BuiltinData -> BuiltinData -> BuiltinData -> BuiltinData -> ()
+mkWrappedDeadlinePolicy pkh deadline = wrapPolicy $ mkDeadlinePolicy (PlutusTx.unsafeFromBuiltinData pkh) (PlutusTx.unsafeFromBuiltinData deadline)
+
+deadlineCode :: PlutusTx.CompiledCode (BuiltinData -> BuiltinData -> BuiltinData -> BuiltinData -> ())
+deadlineCode = $$(PlutusTx.compile [||mkWrappedDeadlinePolicy||])
 
 deadlinePolicy :: PubKeyHash -> POSIXTime -> MintingPolicy
-deadlinePolicy pkh deadline = mkMintingPolicyScript $
-    $$(PlutusTx.compile [|| mkWrappedDeadlinePolicy ||])
-        `PlutusTx.applyCode` PlutusTx.liftCode pkh
-        `PlutusTx.applyCode` PlutusTx.liftCode deadline
+deadlinePolicy pkh deadline =
+  mkMintingPolicyScript $
+    deadlineCode
+      `PlutusTx.applyCode` PlutusTx.liftCode (PlutusTx.toBuiltinData $ pkh)
+      `PlutusTx.applyCode` PlutusTx.liftCode (PlutusTx.toBuiltinData $ deadline)
 
+---------------------------------------------------------------------------------------------------
+------------------------------------- HELPER FUNCTIONS --------------------------------------------
+
+saveDeadlineCode :: IO ()
+saveDeadlineCode = writeCodeToFile "assets/deadline.plutus" deadlineCode
