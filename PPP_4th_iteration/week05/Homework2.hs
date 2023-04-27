4cfaee13bcc675a72c00fc9940b0978d8eadeed4
EyobAshenaki <ethioeyoba@gmail.com>
2023-04-27 09:10:05 +0300

    feat(4th/week05): add serialize on-chain code for hw-2

--- PPP_4th_iteration/week05/Homework2.hs
+++ PPP_4th_iteration/week05/Homework2.hs
@@ -1,39 +1,67 @@
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Homework2 where
 
-import           Plutus.V2.Ledger.Api (BuiltinData, MintingPolicy,
-                                       ScriptContext (scriptContextTxInfo), TokenName, TxOutRef,
-                                       mkMintingPolicyScript, TxInfo, scriptContextTxInfo, txInfoInputs, txInInfoOutRef, txInfoMint, unTokenName)
-import qualified PlutusTx
-import           PlutusTx.Prelude     (Bool (False), ($), (.), traceIfFalse, (==), (&&), any, emptyByteString)
-import           Utilities            (wrapPolicy)
 import Plutus.V1.Ledger.Value (flattenValue)
+import Plutus.V2.Ledger.Api
+  ( BuiltinData,
+    MintingPolicy,
+    ScriptContext (scriptContextTxInfo),
+    TokenName,
+    TxId (TxId),
+    TxInfo,
+    TxOutRef (TxOutRef),
+    mkMintingPolicyScript,
+    scriptContextTxInfo,
+    txInInfoOutRef,
+    txInfoInputs,
+    txInfoMint,
+    unTokenName,
+  )
+import qualified PlutusTx
+import PlutusTx.Prelude (Bool (False), any, emptyByteString, traceIfFalse, ($), (&&), (.), (==))
+import Utilities (wrapPolicy, writeCodeToFile)
+import Prelude (IO)
 
-{-# INLINABLE mkEmptyNFTPolicy #-}
+{-# INLINEABLE mkEmptyNFTPolicy #-}
 -- Minting policy for an NFT, where the minting transaction must consume the given UTxO as input
 -- and where the TokenName will be the empty ByteString.
 mkEmptyNFTPolicy :: TxOutRef -> () -> ScriptContext -> Bool
-mkEmptyNFTPolicy _oref () _ctx = traceIfFalse "UTxO not consumed" hasUTxO &&
-                                 traceIfFalse "TokenName not empty" hasEmptyTokenName
-    where
-        info :: TxInfo
-        info = scriptContextTxInfo _ctx
+mkEmptyNFTPolicy _oref () _ctx =
+  traceIfFalse "UTxO not consumed" hasUTxO
+    && traceIfFalse "TokenName not empty" hasEmptyTokenName
+  where
+    info :: TxInfo
+    info = scriptContextTxInfo _ctx
 
-        hasUTxO :: Bool
-        hasUTxO = any (\i -> txInInfoOutRef i == _oref) $ txInfoInputs info
+    hasUTxO :: Bool
+    hasUTxO = any (\i -> txInInfoOutRef i == _oref) $ txInfoInputs info
 
-        hasEmptyTokenName :: Bool
-        hasEmptyTokenName = case flattenValue (txInfoMint info) of
-            [(_, tn, amt)] -> unTokenName tn == emptyByteString && amt == 1
-            _               -> False
+    hasEmptyTokenName :: Bool
+    hasEmptyTokenName = case flattenValue (txInfoMint info) of
+      [(_, tn, amt)] -> unTokenName tn == emptyByteString && amt == 1
+      _ -> False
 
-{-# INLINABLE mkWrappedEmptyNFTPolicy #-}
-mkWrappedEmptyNFTPolicy :: TxOutRef -> BuiltinData -> BuiltinData -> ()
-mkWrappedEmptyNFTPolicy = wrapPolicy . mkEmptyNFTPolicy
+{-# INLINEABLE mkWrappedEmptyNFTPolicy #-}
+-- mkWrappedEmptyNFTPolicy :: TxOutRef -> BuiltinData -> BuiltinData -> ()
+-- mkWrappedEmptyNFTPolicy = wrapPolicy . mkEmptyNFTPolicy
+mkWrappedEmptyNFTPolicy :: BuiltinData -> BuiltinData -> BuiltinData -> BuiltinData -> ()
+mkWrappedEmptyNFTPolicy tid ix = wrapPolicy $ mkEmptyNFTPolicy oref
+  where
+    oref :: TxOutRef
+    oref = TxOutRef (TxId $ PlutusTx.unsafeFromBuiltinData tid) (PlutusTx.unsafeFromBuiltinData ix)
 
-nftPolicy :: TxOutRef -> TokenName -> MintingPolicy
-nftPolicy oref tn = mkMintingPolicyScript $ $$(PlutusTx.compile [|| mkWrappedEmptyNFTPolicy ||]) `PlutusTx.applyCode` PlutusTx.liftCode oref
+emptyNFTCode :: PlutusTx.CompiledCode (BuiltinData -> BuiltinData -> BuiltinData -> BuiltinData -> ())
+emptyNFTCode = $$(PlutusTx.compile [||mkWrappedEmptyNFTPolicy||])
+
+-- nftPolicy :: TxOutRef -> TokenName -> MintingPolicy
+-- nftPolicy oref tn = mkMintingPolicyScript $ $$(PlutusTx.compile [||mkWrappedEmptyNFTPolicy||]) `PlutusTx.applyCode` PlutusTx.liftCode oref
+
+---------------------------------------------------------------------------------------------------
+------------------------------------- HELPER FUNCTIONS --------------------------------------------
+
+saveEmptyNFTCode :: IO ()
+saveEmptyNFTCode = writeCodeToFile "assets/emptyNFT.plutus" emptyNFTCode
