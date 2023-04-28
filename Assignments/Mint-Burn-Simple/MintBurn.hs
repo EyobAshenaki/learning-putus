8e4653f2a840b130fa757844d4361b4f7baad730
EyobAshenaki <ethioeyoba@gmail.com>
2023-04-28 11:09:43 +0300

    fix(Assignment): patch logical bug on on-chain code

--- Assignments/Mint-Burn-Simple/MintBurn.hs
+++ Assignments/Mint-Burn-Simple/MintBurn.hs
@@ -1,27 +1,37 @@
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
-module Signed where
+module MintBurn where
 
 import Plutus.V1.Ledger.Value (isZero, split)
-import           Plutus.V2.Ledger.Api      (BuiltinData, CurrencySymbol,
-                                            MintingPolicy, PubKeyHash,
-                                            ScriptContext (scriptContextTxInfo),
-                                            mkMintingPolicyScript, TxInfo (txInfoMint))
-import           Plutus.V2.Ledger.Contexts (txSignedBy)
+import Plutus.V2.Ledger.Api
+  ( BuiltinData,
+    CurrencySymbol,
+    MintingPolicy,
+    PubKeyHash,
+    ScriptContext (scriptContextTxInfo),
+    TxInfo (txInfoMint),
+    mkMintingPolicyScript,
+  )
+import Plutus.V2.Ledger.Contexts (txSignedBy)
 import qualified PlutusTx
-import           PlutusTx.Prelude          (Bool (True, False), traceIfFalse, ($), (.), fst, (&&) (||), not)
-import           Prelude                   (IO, Show (show))
-import           Text.Printf               (printf)
-import           Utilities                 (currencySymbol, wrapPolicy,
-                                            writeCodeToFile, writePolicyToFile)
+import PlutusTx.Prelude (Bool (False, True), fst, not, traceIfFalse, ($), (.), (||))
+import Text.Printf (printf)
+import Utilities
+  ( currencySymbol,
+    wrapPolicy,
+    writeCodeToFile,
+    writePolicyToFile,
+  )
+import Prelude (IO, Show (show))
 
-{-# INLINABLE mkMintBurnPolicy #-}
+{-# INLINEABLE mkMintBurnPolicy #-}
 mkMintBurnPolicy :: PubKeyHash -> () -> ScriptContext -> Bool
-mkMintBurnPolicy pkh () ctx = traceIfFalse "*** Missing signature ***" hasSigned && 
-                              traceIfFalse "*** Invalid signature to Mint ***" burnToken
+mkMintBurnPolicy pkh () ctx =
+  traceIfFalse "*** Missing signature ***" hasSigned
+    || traceIfFalse "*** Invalid signature to Mint ***" burnToken
   where
     info :: TxInfo
     info = scriptContextTxInfo ctx
@@ -33,12 +43,12 @@ mkMintBurnPolicy pkh () ctx = traceIfFalse "*** Missing signature ***" hasSigned
     burnToken :: Bool
     burnToken = not $ isZero $ fst $ split $ txInfoMint info
 
-{-# INLINABLE mkWrappedMintBurnPolicy #-}
+{-# INLINEABLE mkWrappedMintBurnPolicy #-}
 mkWrappedMintBurnPolicy :: BuiltinData -> BuiltinData -> BuiltinData -> ()
 mkWrappedMintBurnPolicy pkh = wrapPolicy (mkMintBurnPolicy $ PlutusTx.unsafeFromBuiltinData pkh)
 
 mintBurnCode :: PlutusTx.CompiledCode (BuiltinData -> BuiltinData -> BuiltinData -> ())
-mintBurnCode = $$(PlutusTx.compile [|| mkWrappedMintBurnPolicy ||])
+mintBurnCode = $$(PlutusTx.compile [||mkWrappedMintBurnPolicy||])
 
 mintBurnPolicy :: PubKeyHash -> MintingPolicy
 mintBurnPolicy pkh = mkMintingPolicyScript $ mintBurnCode `PlutusTx.applyCode` PlutusTx.liftCode (PlutusTx.toBuiltinData pkh)
